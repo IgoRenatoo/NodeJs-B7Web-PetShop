9309556ff900db252642301feecafaff0183704a
IgoRenatoo <igor.renato.oliveira@gmail.com>
2024-05-07 20:33:40 -0300

    feat: :sparkles: Introdução Routes, Views e Public/Static

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,18 @@
-import express, { Router, Request, Response } from 'express';
+import express, { Router, Request, Response, urlencoded } from 'express';
 import mustache from 'mustache-express';
 import dotenv from 'dotenv';
 import path from 'path';
-import MainRouters from './routes/router.js';
+import MainRouters from './routes/routes';
 dotenv.config();
 
 const server = express();
 
 server.set('views', path.join(__dirname, 'views'));
 server.set('view engine', 'mustache');
-server.set('mustache', mustache());
+server.engine('mustache', mustache());
 
+server.use(express.static (path.join(__dirname, '../public')));
+server.use(urlencoded({extended:true}))
 server.use(MainRouters);
 
-server.listen(3000);
+server.listen(process.env.PORT);
