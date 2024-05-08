5d0bad1cd13f93ea36723fc2e6565c53c60ad2ce
IgoRenatoo <igor.renato.oliveira@gmail.com>
2024-05-07 23:33:23 -0300

    chore: :wrench: Adicionado Controllers

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,9 +1,12 @@
 import {Router, Request, Response} from 'express';
+import * as PageControllers from '../controllers/pageControllers';
+
 
 const router = Router();
 
-router.get('/', (req: Request, res: Response) => {
-  res.render('pages/home')
-});
+router.get('/', PageControllers.home);
+router.get('/dog', PageControllers.dog);
+router.get('/cat', PageControllers.cat);
+router.get('/fish', PageControllers.fish);
 
 export default router;
